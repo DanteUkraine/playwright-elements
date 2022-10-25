fc1d69b7c4276fc3206333e2dee03753e9f47013
DanteUkraine <osolomin60@outlook.com>
2022-10-25 17:45:04 +0300

    set ts target es2020

--- src/playwright.test.fixtures.ts
+++ src/playwright.test.fixtures.ts
@@ -1,10 +1,10 @@
-import { test as base } from '@playwright/test';
-import { BrowserInstance } from './browser';
+import {test as base} from '@playwright/test';
+import {BrowserInstance} from './browser';
 
-export const test = base.extend<{initElements: BrowserInstance}>({
-    initElements: [async ({ baseURL, page }, use) => {
+export const test = base.extend<{ initElements: BrowserInstance }>({
+    initElements: [async ({baseURL, page}, use) => {
         BrowserInstance.withPage(page);
-        if(baseURL) await BrowserInstance.currentPage.goto(baseURL);
+        if (baseURL) await BrowserInstance.currentPage.goto(baseURL);
         await use(BrowserInstance);
         BrowserInstance.currentPage = undefined;
         BrowserInstance.currentContext = undefined;
