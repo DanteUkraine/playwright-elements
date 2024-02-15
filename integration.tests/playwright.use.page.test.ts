ceaa6a105b2d7949832758c3a36ede226d6d42d5
DanteUkraine <osolomin60@outlook.com>
2024-02-15 09:11:03 +0200

    feature/save

--- integration.tests/playwright.use.page.test.ts
+++ integration.tests/playwright.use.page.test.ts
@@ -1,14 +1,17 @@
 import { test as baseTest, $, usePage, expect } from '../src';
 import { Page } from '@playwright/test';
 
-type TestFixtures = { secondContextPage: Page };
-const test = baseTest.extend<TestFixtures, {}>({
+type TestFixtures = { secondContextPage: Page, useSecondContext: <T>(callback: () => Promise<T>) => Promise<T> };
+const test = baseTest.extend<TestFixtures>({
     secondContextPage: [async ({ browser }, use) => {
         const context = await browser.newContext();
         const page = await context.newPage();
         await use(page);
         await context.close();
-    }, { scope: 'test' }]
+    }, { scope: 'test' }],
+    useSecondContext: [async ({ secondContextPage }, use) => {
+        await use(<T>(callback: () => Promise<T>) => usePage<T>(secondContextPage, callback));
+    }, {  scope: 'test' }]
 });
 
 const title = $('h1');
@@ -40,6 +43,24 @@ test.describe('Playwright test integration', () => {
             return testFixturesPage.title.textContent();
         });
         expect(text).toEqual('Fixtures');
+    });
+
+    test('usePage as fixture returns value', async ({ goto, page, usePage }) => {
+        await goto('/docs/test-fixtures');
+        const text = await usePage<string>(page, async () => {
+            return title.textContent();
+        });
+        expect(text).toEqual('Fixtures');
+    });
+
+    test('usePage wrapped and goto used inside', async ({ goto, useSecondContext }) => {
+        await goto();
+        const text = await useSecondContext<string>(async () => {
+            await goto('/docs/test-fixtures');
+            return title.textContent();
+        });
+        expect(text).toEqual('Fixtures');
+        expect(await title.textContent()).toEqual('Playwright enables reliable end-to-end testing for modern web apps.')
     });
 });
 
