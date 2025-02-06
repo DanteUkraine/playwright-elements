cc78f3be3383adb0a683111dc848780271325363
DanteUkraine <osolomin60@outlook.com>
2025-02-06 22:45:56 +0200

    working prototype done

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ export { BrowserInstance, Context, BrowserName, usePage } from './browser.ts';
 export { WebElement, $, $getByAltText, $getByLabel, $getByPlaceholder, $getByRole, $getByTestId, $getByText,
     $getByTitle, initDesktopOrMobile } from './web.element.js';
 export { test, expect } from './playwright.test.fixtures.js';
-export { buildPageObject, PageObject } from './page.object.builder.js';
+export { buildPageObject } from './page.object.builder.js';
+export type { PageObject } from './page.object.builder.js';
